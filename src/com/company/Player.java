e0ea9c54799d0e21e74e154c2080957727cb4ff0
pygs <pygs1500@gmail.com>
2022-02-11 21:51:49 +0100

    Created Player

---
+++ src/com/company/Player.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player extends Employees{
+    public List<Projects> projectList = new ArrayList<>();
+    public Player(String firstName, String lastName, int java, int frontEnd, int dataBase, int mobile, int wordPress, int prestaShop){
+        super(firstName, lastName, java, frontEnd, dataBase, mobile, wordPress, prestaShop);
+    }
+    public String toString(){
+        return "First name: " + firstName + "\nLast name: " + lastName + "\nJava: " + java + "\nFront-end: " + frontEnd + "\nData Bases: " + dataBase + "\nMobile: " + mobile + "\nWordPress: " + wordPress + "\nPrestaShop: " + prestaShop;
+    }
+
+    public void setProject(Projects projekt){
+        if (this.projectList.isEmpty()) {
+            projectList.add(projekt);
+        }
+        else{
+            System.out.println("You have active project.\n");
+        }
+    }
+
+    public List<Projects> getProject(){
+        return projectList;
+    }
+}
